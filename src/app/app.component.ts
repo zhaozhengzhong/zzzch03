14c1b7ad6eb28b78002b76b168fdd11efeaaaadb
mxhsb <1010195505@qq.com>
2020-04-09 18:30:37 +0800

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,25 +13,25 @@ export class AppComponent {
       new Product(
         'MYSHOES',
         'Black Running Shoes',
-        '/assets/imgs/black-shoes.jpg',
+        './assets/imgs/black-shoes.jpg',
         ['Men', 'Shoes', 'Running Shoes'],
         109.99),
       new Product(
         'NEATOJACKET',
         'Blue Jacket',
-        '/assets/imgs/blue-jacket.jpg',
+        './assets/imgs/blue-jacket.jpg',
         ['Women', 'Apparel', 'Jackets & Vests'], 238.99),
       new Product(
         'NICEHAT',
         'A Nice Black Hat',
-        '/assets/imgs/black-hat.jpg',
+        './assets/imgs/black-hat.jpg',
         ['Men', 'Accessories', 'Hats'],
         29.99)
     ];
 
   }
   onProductSle(product: Product) {
-    console.log('aaaa');
+    // console.log('aaaa');
   }
 }
 
